39f8ebf1f2ac87a725f306b76191e29a07390e67
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-11 13:51:16 +0530

    Lecture-01-Lecture-2-Opeartors git commit -m Lecture-01-Lecture-2-Operators

---
+++ Lecture-02-Operators/Sum_of_Two_Numbers.cpp
@@ -0,0 +1,13 @@
+//Sum of t wo numbers
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, sum;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    sum = a + b; // Using the addition operator to calculate the sum
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
