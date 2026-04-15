1c5aa2f2589e946dfe00ca70609adba27e0a6cf6
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-15 23:19:13 +0530

    SCOPE.CPP

---
+++ Lecture-07-Bitwise-Operator/SCOPE.cpp
@@ -0,0 +1,15 @@
+// Scope is area of code where a variable is visible
+/*
+
+Types of Scope in C/C++
+✅ 1. Local Scope
+Declared inside a function
+
+✅ 2. Block Scope
+Inside {} like if, for, etc.
+
+✅ 3. Global Scope
+Declared outside all functions
+
+
+*/
