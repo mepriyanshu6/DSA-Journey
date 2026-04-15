be8025a72e11e7f46e8ff696711f306ddf0df37b
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-15 23:33:02 +0530

    Arrays.cpp

---
+++ Lecture-08-ARRAYS/Arrays.cpp
@@ -0,0 +1,16 @@
+//An array is a collection of elements of the same type stored in continuous memory locations
+/*
+
+Data Structure => linear and Hirarchial
+(Stores Data)
+
+Algorithms => operation on data
+
+*/
+
+//Arrays Syntax
+/*
+same Type data
+Contiguous in Memory
+Arrays are Linear
+*/
