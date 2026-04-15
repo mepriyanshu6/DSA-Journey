a28f25b28a4bd8ebd5f750e55ddef19daeea44b5
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-15 23:13:08 +0530

    Operator_Precedence.cpp

---
+++ Lecture-07-Bitwise-Operator/Operator_Precedence.cpp
@@ -0,0 +1,22 @@
+/*
+
+| Priority | Operators                    | Description                           | Associativity |
+| -------- | --------------------------   | -----------------------------------   | ------------- |
+| 1️        | `()` `[]` `->` `.`           | Function call, array, member access   | Left → Right  |
+| 2️        | `!` `~` `++` `--` `(type)`   | Unary operators                       | Right → Left  |
+| 3️        | `*` `/` `%`                  | Multiplication, Division, Modulus     | Left → Right  |
+| 4️        | `+` `-`                      | Addition, Subtraction                 | Left → Right  |
+| 5️        | `<<` `>>`                    | Bitwise Shift                         | Left → Right  |
+| 6️        | `<` `<=` `>` `>=`            | Relational                            | Left → Right  |
+| 7️        | `==` `!=`                    | Equality                              | Left → Right  |
+| 8️        | `&`                          | Bitwise AND                           | Left → Right  |
+| 9️        | `^`                          | Bitwise XOR                           | Left → Right  |
+| 10       | `\|`                         | Bitwise OR                            | Left → Right  |
+| 11       | `&&`                         | Logical AND                           | Left → Right  |
+| 12       | `\|\|`                       | Logical OR                            | Left → Right  |
+| 13       | `?:`                         | Ternary operator                      | Right → Left  |
+| 14       | `=` `+=` `-=` etc.           | Assignment                            | Right → Left  |
+| 15       | `,`                          | Comma                                 | Left → Right  |
+
+
+*/
