1936952bfff7fd8706c88e8fb10df14e26127de2
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-04-28 23:34:21 +0530

    vector_front_function.cpp

---
+++ Lecture-09-VECTORS/vector_front_function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec = {1, 2, 3, 4, 5};
+    for ( int i : vec){// for each loop
+        cout << i << endl;
+    }
+    cout << vec.front() <<endl;//to get the first element of the vector.
+    cout << vec.back() <<endl;//to get the last element of the vector.
+    return 0;
+}
