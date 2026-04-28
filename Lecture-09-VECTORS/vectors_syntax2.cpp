b86fdbda5dafdfcd0c18b9aa14eba6dd3efafacb
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-04-28 22:46:25 +0530

    vectors._syntax2.cpp

---
+++ Lecture-09-VECTORS/vectors_syntax2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec(5,0); // 5 is size of vector and o is index value for eachh vector
+    cout << vec [0] << endl;
+    cout << vec [1] << endl;
+    cout << vec [2] << endl;
+    cout << vec [3] << endl;
+    cout << vec [4] << endl;
+    return 0;
+}
